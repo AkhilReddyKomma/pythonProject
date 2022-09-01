1221166e5593b69f7d4eafcf223b992aa5b41cd7
AkhilReddyKomma <axk16540@ucmo.edu>
2022-09-01 13:31:34 -0700

    intiall

--- mainq10.py
+++ mainq10.py
@@ -9,10 +9,10 @@ from sklearn.metrics import confusion_matrix
 X = nm.array([[1, 0], [2, 0], [3, 0], [6, 0], [6, 0], [7, 0], [10, 0], [11, 0]])
 print("Dataset with total 8 points: \n", X, "\n Size: ", len(X))
 y = nm.array([0, 0, 1, 1, 1, 0, 0, 0])
-print("\n Clas label for the datasetX  Y:", y)
+print("\n Clas label for the datasets X  Y:", y)
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.50, random_state=0, shuffle=False)
 print("\nTraining Data X: \n", x_train)
-print("\nTraining Data label Y:", y_train)
+print("\n Training Data label Y:", y_train)
 
 # feature Scaling-used to normalize the range of independent variables or features of data. .
 # i.e.to standardize the data prior to performing classification
